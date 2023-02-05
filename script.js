7ae40c2d3d9ed6bcab7a22658dba46dccc38fb7a
aleikson <aleiksonsilva@hotmail.com>
2023-02-05 01:23:26 -0200

    First Commit

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 function buscarInfo() {
-
     const nomeCientista = document.getElementById("select-cientista").value;
-
     fetch(`https://pt.wikipedia.org/api/rest_v1/page/summary/${nomeCientista}`)
         .then(response => response.json())
         .then(data => {
